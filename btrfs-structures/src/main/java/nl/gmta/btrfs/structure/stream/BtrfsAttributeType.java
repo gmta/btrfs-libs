60b05ea919b3c14869448d297d1a7130b4c38825
Jelle Raaijmakers <jelle@gmta.nl>
2017-04-02 01:15:28 +0200

    BtrfsAttributeType: map value types onto attribute types

--- btrfs-structures/src/main/java/nl/gmta/btrfs/structure/stream/BtrfsAttributeType.java
+++ btrfs-structures/src/main/java/nl/gmta/btrfs/structure/stream/BtrfsAttributeType.java
@@ -1,38 +1,44 @@
 package nl.gmta.btrfs.structure.stream;
 
 enum BtrfsAttributeType {
-    UUID           (0x01),
-    CTRANSID       (0x02),
-    INO            (0x03),
-    SIZE           (0x04),
-    MODE           (0x05),
-    UID            (0x06),
-    GID            (0x07),
-    RDEV           (0x08),
-    CTIME          (0x09),
-    MTIME          (0x0A),
-    ATIME          (0x0B),
-    OTIME          (0x0C),
-    XATTR_NAME     (0x0D),
-    XATTR_DATA     (0x0E),
-    PATH           (0x0F),
-    PATH_TO        (0x10),
-    PATH_LINK      (0x11),
-    FILE_OFFSET    (0x12),
-    DATA           (0x13),
-    CLONE_UUID     (0x14),
-    CLONE_CTRANSID (0x15),
-    CLONE_PATH     (0x16),
-    CLONE_OFFSET   (0x17),
-    CLONE_LEN      (0x18);
+    UUID           (0x01, BtrfsValueType.UUID),
+    CTRANSID       (0x02, BtrfsValueType.U64),
+    INO            (0x03, BtrfsValueType.U64),
+    SIZE           (0x04, BtrfsValueType.U64),
+    MODE           (0x05, BtrfsValueType.U64),
+    UID            (0x06, BtrfsValueType.U64),
+    GID            (0x07, BtrfsValueType.U64),
+    RDEV           (0x08, BtrfsValueType.U64),
+    CTIME          (0x09, BtrfsValueType.TIMESPEC),
+    MTIME          (0x0A, BtrfsValueType.TIMESPEC),
+    ATIME          (0x0B, BtrfsValueType.TIMESPEC),
+    OTIME          (0x0C, BtrfsValueType.TIMESPEC),
+    XATTR_NAME     (0x0D, BtrfsValueType.STRING),
+    XATTR_DATA     (0x0E, BtrfsValueType.BINARY),
+    PATH           (0x0F, BtrfsValueType.STRING),
+    PATH_TO        (0x10, BtrfsValueType.STRING),
+    PATH_LINK      (0x11, BtrfsValueType.STRING),
+    FILE_OFFSET    (0x12, BtrfsValueType.U64),
+    DATA           (0x13, BtrfsValueType.BINARY),
+    CLONE_UUID     (0x14, BtrfsValueType.UUID),
+    CLONE_CTRANSID (0x15, BtrfsValueType.U64),
+    CLONE_PATH     (0x16, BtrfsValueType.STRING),
+    CLONE_OFFSET   (0x17, BtrfsValueType.U64),
+    CLONE_LEN      (0x18, BtrfsValueType.U64);
 
     private int value;
+    private BtrfsValueType type;
 
-    BtrfsAttributeType(int value) {
+    BtrfsAttributeType(int value, BtrfsValueType type) {
         this.value = value;
+        this.type = type;
     }
 
     public int getValue() {
         return this.value;
+    }
+
+    public BtrfsValueType getType() {
+        return this.type;
     }
 }
--- btrfs-structures/src/main/java/nl/gmta/btrfs/structure/stream/BtrfsValueType.java
+++ btrfs-structures/src/main/java/nl/gmta/btrfs/structure/stream/BtrfsValueType.java
@@ -1,22 +1,9 @@
 package nl.gmta.btrfs.structure.stream;
 
 enum BtrfsValueType {
-    U8       (0x00),
-    U16      (0x01),
-    U32      (0x02),
-    U64      (0x03),
-    BINARY   (0x04),
-    STRING   (0x05),
-    UUID     (0x06),
-    TIMESPEC (0x07);
-
-    private int value;
-
-    BtrfsValueType(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return this.value;
-    }
+    U64,
+    BINARY,
+    STRING,
+    UUID,
+    TIMESPEC;
 }
