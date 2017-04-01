93dc908064e4fa00eb76649ca86d2e563f39e0cb
Jelle Raaijmakers <jelle@gmta.nl>
2017-04-02 01:03:45 +0200

    BtrfsStreamCommand: add command type and CRC value

--- btrfs-structures/src/main/java/nl/gmta/btrfs/structure/stream/BtrfsStreamCommand.java
+++ btrfs-structures/src/main/java/nl/gmta/btrfs/structure/stream/BtrfsStreamCommand.java
@@ -2,12 +2,24 @@ package nl.gmta.btrfs.structure.stream;
 
 public abstract class BtrfsStreamCommand extends BtrfsStreamElement {
     private final int length;
+    private final BtrfsCommandType command;
+    private final int crc;
 
-    public BtrfsStreamCommand(int length) {
+    public BtrfsStreamCommand(int length, BtrfsCommandType command, int crc) {
         this.length = length;
+        this.command = command;
+        this.crc = crc;
     }
 
     public int getLength() {
         return this.length;
+    }
+
+    public BtrfsCommandType getCommand() {
+        return this.command;
+    }
+
+    public int getCrc() {
+        return this.crc;
     }
 }
