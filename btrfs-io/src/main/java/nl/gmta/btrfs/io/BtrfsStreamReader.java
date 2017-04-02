ea2ecb041ae8909b5b16bbf07f92bfa472d6e60b
Jelle Raaijmakers <jelle@gmta.nl>
2017-04-02 15:33:10 +0200

    BtrfsStreamReader: move data interpretation to new DataReader

--- btrfs-io/src/main/java/nl/gmta/btrfs/io/BtrfsStreamReader.java
+++ btrfs-io/src/main/java/nl/gmta/btrfs/io/BtrfsStreamReader.java
@@ -22,21 +22,20 @@ public class BtrfsStreamReader implements AutoCloseable {
     private static final int VALUE_TYPE_UUID_SIZE = 16;
     private static final int SUPPORTED_VERSION = 1;
 
-    private final InputStream is;
+    private final DataReader reader;
     private boolean isHeaderRead = false;
-    private long position = 0;
 
     public BtrfsStreamReader(InputStream is) {
-        this.is = is;
+        this.reader = new DataReader(is);
     }
 
     @Override
     public void close() throws IOException {
-        this.is.close();
+        this.reader.close();
     }
 
     public boolean hasNext() throws IOException {
-        return (this.is.available() > 0);
+        return (this.reader.available() > 0);
     }
 
     public BtrfsStreamElement next() throws IOException {
@@ -45,79 +44,48 @@ public class BtrfsStreamReader implements AutoCloseable {
 
     private Object readAttribute(BtrfsAttributeType type) throws IOException {
         // Read and verify attribute type
-        int readType = this.readLE16();
+        int readType = this.reader.readLE16();
         if (readType != type.getId()) {
             throw new BtrfsStructureException(String.format("Expected attribute type %04X but got %04X", type.getId(), readType));
         }
 
         // Read attribute value
-        int length = this.readLE16();
+        int length = this.reader.readLE16();
         Object result;
         switch (type.getType()) {
             case BINARY:
-                result = this.readBytes(length);
+                result = this.reader.readBytes(length);
                 break;
             case STRING:
-                byte[] stringData = this.readBytes(length);
+                byte[] stringData = this.reader.readBytes(length);
                 result = new String(stringData, StandardCharsets.US_ASCII);
                 break;
             case TIMESPEC:
                 if (length != VALUE_TYPE_TIMESPEC_SIZE) {
                     throw new BtrfsStructureException(String.format("Unexpected timespec size: %d", length));
                 }
-                long seconds = this.readLE64();
-                int nanoSeconds = this.readLE32();
+                long seconds = this.reader.readLE64();
+                int nanoSeconds = this.reader.readLE32();
                 result = new BtrfsTimespec(seconds, nanoSeconds);
                 break;
             case U64:
                 if (length != VALUE_TYPE_U64_SIZE) {
                     throw new BtrfsStructureException(String.format("Unexpected U64 size: %d", length));
                 }
-                result = this.readLE64();
+                result = this.reader.readLE64();
                 break;
             case UUID:
                 if (length != VALUE_TYPE_UUID_SIZE) {
                     throw new BtrfsStructureException(String.format("Unexpected UUID size: %d", length));
                 }
-                long mostSigUUID = this.readBE64();
-                long leastSigUUID = this.readBE64();
+                long mostSigUUID = this.reader.readBE64();
+                long leastSigUUID = this.reader.readBE64();
                 result = new UUID(mostSigUUID, leastSigUUID);
                 break;
             default:
                 throw new BtrfsStructureException(String.format("Unimplemented value type: %s", type.getType()));
         }
         return result;
-    }
-
-    private long readBE64() throws IOException {
-        byte[] longBytes = this.readBytes(8);
-        return (longBytes[7] & 0xFF)
-            | ((longBytes[6] & 0xFF) << 8)
-            | ((longBytes[5] & 0xFF) << 16)
-            | ((long) (longBytes[4] & 0xFF) << 24)
-            | ((long) (longBytes[3] & 0xFF) << 32)
-            | ((long) (longBytes[2] & 0xFF) << 40)
-            | ((long) (longBytes[1] & 0xFF) << 48)
-            | ((long) (longBytes[0] & 0xFF) << 56);
-    }
-
-    private byte readByte() throws IOException {
-        byte result = (byte) this.is.read();
-        ++this.position;
-        return result;
-    }
-
-    private byte[] readBytes(int length) throws IOException {
-        byte[] buffer = new byte[length];
-        int read = this.is.read(buffer);
-        if (read < 0) {
-            throw new BtrfsStructureException("Unexpected EOF while reading btrfs stream");
-        } else if (read != length) {
-            throw new BtrfsStructureException("Invalid number of bytes read");
-        }
-
-        this.position += read;
-        return buffer;
     }
 
     private BtrfsStreamCommand readCommand() throws IOException {
@@ -146,15 +114,15 @@ public class BtrfsStreamReader implements AutoCloseable {
     }
 
     private BtrfsStreamCommandHeader readCommandHeader() throws IOException {
-        int length = this.readLE32();
+        int length = this.reader.readLE32();
 
-        int commandValue = this.readLE16();
+        int commandValue = this.reader.readLE16();
         BtrfsCommandType command = BtrfsCommandType.getById(commandValue);
         if (command == null) {
             throw new BtrfsStructureException(String.format("Invalid command type: %d", commandValue));
         }
 
-        int crc = this.readLE32();
+        int crc = this.reader.readLE32();
 
         return new BtrfsStreamCommandHeader(length, command, crc);
     }
@@ -170,45 +138,19 @@ public class BtrfsStreamReader implements AutoCloseable {
     private BtrfsStreamHeader readHeader() throws IOException {
         // Read and verify magic (and NUL trail)
         byte[] magicExpect = BtrfsStreamHeader.MAGIC.getBytes(StandardCharsets.US_ASCII);
-        byte[] magicActual = this.readBytes(magicExpect.length);
-        byte nulTrail = this.readByte();
+        byte[] magicActual = this.reader.readBytes(magicExpect.length);
+        byte nulTrail = this.reader.readByte();
         if (!Arrays.equals(magicExpect, magicActual) || (nulTrail != 0)) {
             throw new BtrfsStructureException("No btrfs stream header magic found");
         }
 
         // Read and verify version
-        int version = this.readLE32();
+        int version = this.reader.readLE32();
         if (version != SUPPORTED_VERSION) {
             throw new BtrfsStructureException(String.format("Unsupported btrfs stream version: %d", version));
         }
 
         this.isHeaderRead = true;
         return new BtrfsStreamHeader(version);
-    }
-
-    private int readLE16() throws IOException {
-        byte[] intBytes = this.readBytes(2);
-        return (intBytes[0] & 0xFF)
-            | ((intBytes[1] & 0xFF) << 8);
-    }
-
-    private int readLE32() throws IOException {
-        byte[] intBytes = this.readBytes(4);
-        return (intBytes[0] & 0xFF)
-            | ((intBytes[1] & 0xFF) << 8)
-            | ((intBytes[2] & 0xFF) << 16)
-            | ((intBytes[3] & 0xFF) << 24);
-    }
-
-    private long readLE64() throws IOException {
-        byte[] longBytes = this.readBytes(8);
-        return (longBytes[0] & 0xFF)
-            | ((longBytes[1] & 0xFF) << 8)
-            | ((longBytes[2] & 0xFF) << 16)
-            | ((long) (longBytes[3] & 0xFF) << 24)
-            | ((long) (longBytes[4] & 0xFF) << 32)
-            | ((long) (longBytes[5] & 0xFF) << 40)
-            | ((long) (longBytes[6] & 0xFF) << 48)
-            | ((long) (longBytes[7] & 0xFF) << 56);
     }
 }
---
+++ btrfs-io/src/main/java/nl/gmta/btrfs/io/DataReader.java
@@ -0,0 +1,85 @@
+package nl.gmta.btrfs.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import nl.gmta.btrfs.io.exception.BtrfsStructureException;
+
+class DataReader implements AutoCloseable {
+    private final InputStream is;
+    private long position = 0;
+
+    DataReader(InputStream is) {
+        this.is = is;
+    }
+
+    int available() throws IOException {
+        return this.is.available();
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.is.close();
+    }
+
+    long getPosition() {
+        return this.position;
+    }
+
+    long readBE64() throws IOException {
+        byte[] longBytes = this.readBytes(8);
+        return (longBytes[7] & 0xFF)
+            | ((longBytes[6] & 0xFF) << 8)
+            | ((longBytes[5] & 0xFF) << 16)
+            | ((long) (longBytes[4] & 0xFF) << 24)
+            | ((long) (longBytes[3] & 0xFF) << 32)
+            | ((long) (longBytes[2] & 0xFF) << 40)
+            | ((long) (longBytes[1] & 0xFF) << 48)
+            | ((long) (longBytes[0] & 0xFF) << 56);
+    }
+
+    byte readByte() throws IOException {
+        byte result = (byte) this.is.read();
+        ++this.position;
+        return result;
+    }
+
+    byte[] readBytes(int length) throws IOException {
+        byte[] buffer = new byte[length];
+        int read = this.is.read(buffer);
+        if (read < 0) {
+            throw new BtrfsStructureException("Unexpected EOF while reading btrfs stream");
+        } else if (read != length) {
+            throw new BtrfsStructureException("Invalid number of bytes read");
+        }
+
+        this.position += read;
+        return buffer;
+    }
+
+    int readLE16() throws IOException {
+        byte[] intBytes = this.readBytes(2);
+        return (intBytes[0] & 0xFF)
+            | ((intBytes[1] & 0xFF) << 8);
+    }
+
+    int readLE32() throws IOException {
+        byte[] intBytes = this.readBytes(4);
+        return (intBytes[0] & 0xFF)
+            | ((intBytes[1] & 0xFF) << 8)
+            | ((intBytes[2] & 0xFF) << 16)
+            | ((intBytes[3] & 0xFF) << 24);
+    }
+
+    long readLE64() throws IOException {
+        byte[] longBytes = this.readBytes(8);
+        return (longBytes[0] & 0xFF)
+            | ((longBytes[1] & 0xFF) << 8)
+            | ((longBytes[2] & 0xFF) << 16)
+            | ((long) (longBytes[3] & 0xFF) << 24)
+            | ((long) (longBytes[4] & 0xFF) << 32)
+            | ((long) (longBytes[5] & 0xFF) << 40)
+            | ((long) (longBytes[6] & 0xFF) << 48)
+            | ((long) (longBytes[7] & 0xFF) << 56);
+    }
+}
